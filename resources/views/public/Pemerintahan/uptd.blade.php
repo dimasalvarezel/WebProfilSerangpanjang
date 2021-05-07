f1293f45843c21e81050f3fc905e56851076dd6a
dimasalvarezel <dimasalvarezel@gmail.com>
2021-05-07 09:15:35 +0700

    7mei2021

--- resources/views/public/Pemerintahan/uptd.blade.php
+++ resources/views/public/Pemerintahan/uptd.blade.php
@@ -15,7 +15,7 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-12 col-md-12 col-lg-12">
-          <h1 class="pagetitle__heading">UPTD</h1>
+          <h1 class="pagetitle__heading">UPTD Serangpanjang</h1>
         </div><!-- /.col-lg-12 -->
       </div><!-- /.row -->
     </div><!-- /.container -->
